f149260f02b289e53acaac4ff8d5e54bcfae5e2d
Himanshu <him.10meena@gmail.com>
2021-09-22 23:40:30 +0530

    using mutationObserver

--- mutify.js
+++ mutify.js
@@ -1,33 +1,22 @@
-const observer = new IntersectionObserver(
-  (entries, self) => {
-    entries.forEach((entry) => {
-      console.log(entry);
-      if (entry.intersectionRatio > 0) {
-        // ads visible mute
-        mute(true);
-      } else {
-        // ads gone unmute
-        mute(false);
-      }
-    });
-  },
-  {
-    root: null,
-    rootMargin: "0px",
-    threshold: 1,
-  }
-);
+// Select the node that will be observed for mutations
+const targetNode = document.getElementsByClassName("i0XB7255K_4QFLJsSGc_")[0];
 
-observer.observe(
-  document.querySelector('[data-testid="CoverSlotExpanded__container"]')
-);
+// Options for the observer (which mutations to observe)
+const config = { childList: true };
 
-function mute(shouldMute) {
-  const isMuted =
-    document
-      .getElementsByClassName("volume-bar__icon-button")[0]
-      .getAttribute("aria-label") === "Unmute";
-  if ((shouldMute && !isMuted) || (!shouldMute && isMuted)) {
-    document.getElementsByClassName("volume-bar__icon-button")[0].click();
+// Callback function to execute when mutations are observed
+const callback = function (mutationsList, observer) {
+  // Use traditional 'for loops' for IE 11
+  for (const mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      console.log("A child node has been added or removed.");
+      document.getElementsByClassName("volume-bar__icon-button")[0].click();
+    }
   }
-}
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
