6562492f0f970876c564dd0f7eb85aff0311744d
Himanshu <him.10meena@gmail.com>
2021-10-08 20:34:35 +0530

    start alert added, multiple run fixes

--- browserExtension.js
+++ browserExtension.js
@@ -1,35 +1,49 @@
 javascript: (function () {
+  console.log("dfdf");
   try {
-  const nextSongBtn = document.querySelector(
-    "[data-testid='control-button-skip-forward']"
-  );
-  const config = { attributes: true };
+    console.log("window.mutify", window.mutify);
+    if (window.mutify) {
+      alert("already observing......");
+      return;
+    }
+    const nextSongBtn = document.querySelector(
+      "[data-testid='control-button-skip-forward']"
+    );
+    const config = { attributes: true };
 
-  const callback = function (mutationsList, observer) {
-    for (const mutation of mutationsList) {
-      if (mutation.type === "attributes") {
-        isNextSongBtnDisabled = nextSongBtn.hasAttribute("disabled");
-        const volumeIcon = document.getElementsByClassName(
-          "volume-bar__icon-button"
-        )[0];
-        let isMuted = volumeIcon.getAttribute("title") === "Unmute";
+    const callback = function (mutationsList) {
+      for (const mutation of mutationsList) {
+        if (mutation.type === "attributes") {
+          const isNextSongBtnDisabled = nextSongBtn.hasAttribute("disabled");
+          const volumeIcon = document.getElementsByClassName(
+            "volume-bar__icon-button"
+          )[0];
+          let isMuted = volumeIcon.getAttribute("aria-label") === "Unmute";
 
-        if (isNextSongBtnDisabled && !isMuted) {
-          // ads playing, next song btn disabled => mute audio
-          console.log("mute audio..........");
-          volumeIcon.click();
-        } else if (!isNextSongBtnDisabled && isMuted) {
-          // ads gone, next song btn enabled => unmute audio
-          console.log("unmute audio..........");
-          volumeIcon.click();
+          console.log(
+            `isNextSongBtnDisabled`,
+            nextSongBtn,
+            isNextSongBtnDisabled
+          );
+          console.log(`isMuted`, volumeIcon, isMuted);
+          if (isNextSongBtnDisabled && !isMuted) {
+            console.log("mute audio..........");
+            volumeIcon.click();
+          } else if (!isNextSongBtnDisabled && isMuted) {
+            console.log("unmute audio..........");
+            volumeIcon.click();
+          }
         }
       }
-    }
-  };
+    };
 
-  const observer = new MutationObserver(callback);
-  observer.observe(nextSongBtn, config);
-  console.log("observing...........");
+    const spotifyObserver = new MutationObserver(callback);
+    if (nextSongBtn) {
+      spotifyObserver.observe(nextSongBtn, config);
+      window.mutify = true;
+      console.log("observing...........");
+      alert("Mutify started !!");
+    }
   } catch (err) {
     console.log("oopss....", err);
   }
