6a0594d97acdc0ec8a08cab8065af49ce484d053
himanshu meena <himanshu.m.meena@oracle.com>
2022-01-30 11:42:12 +0530

    mute at launch added

--- browserExtension.js
+++ browserExtension.js
@@ -1,16 +1,20 @@
 javascript: (function () {
-  console.log("dfdf");
   try {
     console.log("window.mutify", window.mutify);
     if (window.mutify) {
       alert("already observing......");
       return;
     }
+
     const nextSongBtn = document.querySelector(
       "[data-testid='control-button-skip-forward']"
     );
-    const config = { attributes: true };
+    console.log("Select the node that will be observed for mutations",nextSongBtn);
 
+    const config = { attributes: true };
+    console.log("Options for the observer (which mutations to observe)", config);
+
+    console.log("Callback function to execute when mutations are observed");
     const callback = function (mutationsList) {
       for (const mutation of mutationsList) {
         if (mutation.type === "attributes") {
@@ -37,11 +41,25 @@ javascript: (function () {
       }
     };
 
+    console.log("Create an observer instance linked to the callback function");
     const spotifyObserver = new MutationObserver(callback);
+
     if (nextSongBtn) {
+      console.log("Start observing the target node for configured mutations");
       spotifyObserver.observe(nextSongBtn, config);
       window.mutify = true;
       console.log("observing...........");
+
+      const isNextSongBtnDisabled = nextSongBtn.hasAttribute("disabled");
+      const volumeIcon = document.getElementsByClassName(
+        "volume-bar__icon-button"
+      )[0];
+
+      let isMuted = volumeIcon.getAttribute("aria-label") === "Unmute";
+      if (isNextSongBtnDisabled && !isMuted) {
+        console.log("intialize mute audio..........");
+        volumeIcon.click();
+      }
       alert("Mutify started !!");
     }
   } catch (err) {
